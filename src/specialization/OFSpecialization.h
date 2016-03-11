9b1f76661c28b268da809e93c1f7887ec909bbed
DaitoManabe <daito@rhizomatiks.com>
2016-03-11 22:59:19 +0900

    fixed. lerpT amout is float

--- src/specialization/OFSpecialization.h
+++ src/specialization/OFSpecialization.h
@@ -2,12 +2,12 @@
 
 #include "choreograph/Sequence.hpp"
 #include "ofQuaternion.h"
-/// Specialization of lerpT for openFrameworks.
+///Specialization of lerpT for openFrameworks.
 ///ofQuaternion,ofColor,ofFloatColor and ofShortColor.
 namespace  choreograph
 {
     template<>
-    inline ofQuaternion lerpT( const ofQuaternion &start, const ofQuaternion &end, Time time )
+    inline ofQuaternion lerpT( const ofQuaternion &start, const ofQuaternion &end, float time )
     {
         ofQuaternion ret;
         ret.slerp(time, start, end);
@@ -16,11 +16,10 @@ namespace  choreograph
     }
 #define LERP_COLOR(TYPE)\
 template<>\
-inline TYPE lerpT(const TYPE & start, const TYPE & end, Time time){\
+inline TYPE lerpT(const TYPE & start, const TYPE & end, float time){\
 return start.getLerped(end, time);}
     LERP_COLOR(ofColor)
     LERP_COLOR(ofFloatColor)
     LERP_COLOR(ofShortColor)
 #undef LERP_COLOR
-
 } // namespace choreograph
